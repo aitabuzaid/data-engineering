d44bd6639614eb4fbee03d90bc529ac1d72113e2
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-01-08 21:34:37 +0300

    Added queries to create, dro insert JSON data to the songs table

--- Project 1 Postgres/sql_queries.py
+++ Project 1 Postgres/sql_queries.py
@@ -3,7 +3,7 @@
 songplay_table_drop = "DROP TABLE IF EXISTS songs"
 user_table_drop = ""
 song_table_drop = ""
-artist_table_drop = ""
+artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = ""
 
 # CREATE TABLES
@@ -21,7 +21,11 @@ user_table_create = ("""
 song_table_create = ("""
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar,
+                                                              artist_name varchar,
+                                                              artist_location varchar, 
+                                                              artist_latitude float,
+                                                              artist_longitude float);
 """)
 
 time_table_create = ("""
@@ -39,7 +43,8 @@ song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, dura
                         VALUES(%s, %s, %s, %s, %s)
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
+                          VALUES(%s, %s, %s, %s, %s)
 """)
 
 
@@ -52,7 +57,7 @@ song_select = ("""
 """)
 
 # QUERY LISTS
-create_table_queries = [songplay_table_create]
-drop_table_queries = [songplay_table_drop]
+create_table_queries = [songplay_table_create, artist_table_create]
+drop_table_queries = [songplay_table_drop, artist_table_create]
 #create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 #drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
