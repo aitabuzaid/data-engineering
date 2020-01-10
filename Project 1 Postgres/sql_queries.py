a7d7f42d2f1efef7cbd6a80a57e96627bca03e42
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-01-10 21:01:26 +0300

    Added queries to create, drop, and insert JSON data to the users table

--- Project 1 Postgres/sql_queries.py
+++ Project 1 Postgres/sql_queries.py
@@ -1,24 +1,28 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songs"
-user_table_drop = ""
-song_table_drop = ""
+songplay_table_drop = ""
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = ""
 
 # CREATE TABLES
-
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar,
-                                                              title varchar,
-                                                              artist_id varchar, 
-                                                              year int,
-                                                              duration float);
+ 
+songplay_table_create = ("""
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int,
+                                                          first_name varchar,
+                                                          last_name varchar, 
+                                                          gender varchar,
+                                                          level varchar);
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar,
+                                                          title varchar,
+                                                          artist_id varchar, 
+                                                          year int,
+                                                          duration float);
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar,
@@ -36,7 +40,8 @@ time_table_create = ("""
 songplay_table_insert = ("""
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+                        VALUES(%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
@@ -57,7 +62,7 @@ song_select = ("""
 """)
 
 # QUERY LISTS
-create_table_queries = [songplay_table_create, artist_table_create]
-drop_table_queries = [songplay_table_drop, artist_table_create]
+create_table_queries = [song_table_create, artist_table_create, user_table_create]
+drop_table_queries = [song_table_drop, artist_table_drop, user_table_drop]
 #create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 #drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
