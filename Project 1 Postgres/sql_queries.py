020d15a06a01b57a394e0145e04bffc8a54eda6f
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-01-10 22:06:16 +0300

    Added queries to create, drop, and insert JSON data to the time table

--- Project 1 Postgres/sql_queries.py
+++ Project 1 Postgres/sql_queries.py
@@ -4,7 +4,7 @@ songplay_table_drop = ""
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = ""
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
  
@@ -31,8 +31,14 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar,
                                                               artist_latitude float,
                                                               artist_longitude float);
 """)
-
-time_table_create = ("""
+['Time Stamp', 'Hour', 'Day', 'Week of Year', 'Month', 'Year', 'Weekday']
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (time_stamp timestamp,
+                                                         hour int,
+                                                         day int,
+                                                         week_of_year int,
+                                                         month int,
+                                                         year int,
+                                                         weekday int);
 """)
 
 # INSERT RECORDS
@@ -53,7 +59,8 @@ artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_lo
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (time_stamp, hour, day, week_of_year, month, year, weekday)
+                        VALUES(%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
@@ -62,7 +69,7 @@ song_select = ("""
 """)
 
 # QUERY LISTS
-create_table_queries = [song_table_create, artist_table_create, user_table_create]
-drop_table_queries = [song_table_drop, artist_table_drop, user_table_drop]
+create_table_queries = [time_table_create, song_table_create, artist_table_create, user_table_create]
+drop_table_queries = [time_table_drop, song_table_drop, artist_table_drop, user_table_drop]
 #create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 #drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
