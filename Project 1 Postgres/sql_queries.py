ee99da2bbf8bee070b09259397abd524f1ff21db
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-01-11 20:17:49 +0300

    Fixed the insert queries to prevent duplicarte

--- Project 1 Postgres/sql_queries.py
+++ Project 1 Postgres/sql_queries.py
@@ -22,21 +22,24 @@ user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int,
                                                           first_name varchar,
                                                           last_name varchar, 
                                                           gender varchar,
-                                                          level varchar);
+                                                          level varchar,
+                                                          UNIQUE(user_id));
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar,
                                                           title varchar,
                                                           artist_id varchar, 
                                                           year int,
-                                                          duration float);
+                                                          duration float,
+                                                          UNIQUE(song_id));
 """)
 
 artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar,
                                                               artist_name varchar,
                                                               artist_location varchar, 
                                                               artist_latitude float,
-                                                              artist_longitude float);
+                                                              artist_longitude float,
+                                                              UNIQUE(artist_id));
 """)
 ['Time Stamp', 'Hour', 'Day', 'Week of Year', 'Month', 'Year', 'Weekday']
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (time_stamp timestamp,
@@ -51,31 +54,31 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (time_stamp timestamp,
 # INSERT RECORDS
 
 songplay_table_insert = ("""INSERT INTO songplays (time_stamp, user_id, level, song_id, artist_id, session_id, location, user_agent)
-                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
-                        VALUES(%s, %s, %s, %s, %s)
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
+                        VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING;
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
-                        VALUES(%s, %s, %s, %s, %s)
+                        VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
-                          VALUES(%s, %s, %s, %s, %s)
+                          VALUES(%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""INSERT INTO time (time_stamp, hour, day, week_of_year, month, year, weekday)
-                        VALUES(%s, %s, %s, %s, %s, %s, %s)
+                        VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""SELECT songs.song_id, artists.artist_id FROM
                          songs JOIN artists ON songs.artist_id = artists.artist_id
-                  WHERE songs.title = %s AND artists.artist_name = %s AND songs.duration = %s
+                  WHERE (songs.title = %s AND artists.artist_name = %s AND songs.duration = %s)
 """)
 
 # QUERY LISTS
