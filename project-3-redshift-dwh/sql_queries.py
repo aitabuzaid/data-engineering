63bd6240a1964ac310210e29fa898158257d5dfe
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-01-28 19:57:35 +0300

    Added queries to create staging tables

--- project-3-redshift-dwh/sql_queries.py
+++ project-3-redshift-dwh/sql_queries.py
@@ -7,23 +7,61 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = 'DROP TABLE IF EXISTS staging_events'
+staging_songs_table_drop = 'DROP TABLE IF EXISTS staging_songs'
+songplay_table_drop = 'DROP TABLE IF EXISTS songplays'
+user_table_drop = 'DROP TABLE IF EXISTS users'
+song_table_drop = 'DROP TABLE IF EXISTS songs'
+artist_table_drop = 'DROP TABLE IF EXISTS artists'
+time_table_drop = 'DROP TABLE IF EXISTS time'
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events (
+    artist          VARCHAR,
+    auth            VARCHAR,
+    first_name      VARCHAR,
+    gender          VARCHAR(1),
+    item_in_session INTEGER,
+    last_name       VARCHAR,
+    length          NUMERIC,
+    level           VARCHAR(4),
+    location        VARCHAR,
+    method          VARCHAR(3),
+    page            VARCHAR,
+    registration    INTEGER,
+    session_id      INTEGER,
+    song            VARCHAR,
+    status          INTEGER,
+    time_stamp      DATETIME,
+    user_agent      VARCHAR
+    user_id         INTEGER,
+    );
 """)
 
-staging_songs_table_create = ("""
+staging_songs_table_create = ("""CREATE TABLE IF NOT EXISTS staging_songs (
+    num_songs       INTEGER,
+    artist_id       VARCHAR,
+    artist_latitude NUMERIC,
+    artist_longitude NUMERIC,
+    artist_location VARCHAR,
+    artist_name     VARCHAR,
+    song_id         VARCHAR,
+    title           VARCHAR,
+    duration        NUMERIC,
+    year            INTEGER,);
 """)
 
-songplay_table_create = ("""
+songplay_table_create= ("""CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id     INTEGER,
+    start_time      DATETIME,
+    user_id         INTEGER,
+    level           VARCHAR,
+    song_id         VARCHAR,
+    artist_id       VARCHAR,
+    session_id      INTEGER,
+    location        VARCHAR,
+    user_agent      VARCHAR);
 """)
 
 user_table_create = ("""
