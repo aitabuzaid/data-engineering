4ed88bac6b7456806e8e9f409913a7cf505b66bc
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-07 20:16:54 +0300

    Added documentation for create_tables

--- project-3-redshift-dwh/create_tables.py
+++ project-3-redshift-dwh/create_tables.py
@@ -7,6 +7,13 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    A function that that drops all the tables specified in the sql_queries module
+    
+    Keyword arguments:
+    cur -- a cursor to allow running sql commands through the connection
+    conn -- a connection to the Postgres database
+    """
     print("Dropping tables:")
     i = 1
     for query in drop_table_queries:
@@ -17,6 +24,13 @@ def drop_tables(cur, conn):
 
 
 def create_tables(cur, conn):
+    """
+    A function that creates all the tables specified in the sql_queries module
+    
+    Keyword arguments:
+    cur -- a cursor to allow running sql commands through the connection
+    conn -- a connection to the Postgres database
+    """
     print("Creating tables:")
     i = 1
     for query in create_table_queries:
@@ -27,17 +41,18 @@ def create_tables(cur, conn):
 
 
 def main():
+    """Loads the configuration file and connects to the redshift cluster
+     Subsequently, it drops all the tables in the cluster and recreates them
+    """
+    # load the configuration file
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
     
-    
-    
+    # connect to redshift cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    #conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(DWH_ENDPOINT, DWH_DB, DWH_DB_USER ,DWH_DB_PASSWORD, DWH_PORT))
-    
     cur = conn.cursor()
-    
-    
+        
+    # drop the table if they exist and then create them
     drop_tables(cur, conn)
     create_tables(cur, conn)
     
