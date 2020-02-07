f7e2b07fd2d0f58857e6061e2b069a5d17d67b6a
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-07 20:30:36 +0300

    Added documentation for manage_cluster

--- project-3-redshift-dwh/manage_cluster.py
+++ project-3-redshift-dwh/manage_cluster.py
@@ -4,13 +4,25 @@ import json
 import pandas as pd
 
 def prettyRedshiftProps(props):
-        pd.set_option('display.max_colwidth', -1)
-        keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
-        x = [(k, v) for k,v in props.items() if k in keysToShow]
-        return pd.DataFrame(data=x, columns=["Key", "Value"])
+    """Prints the properties of the redshift cluster in a visually appealing table.
+    
+    Keyword arguments:
+    props -- a dictionary of the props of the redshift cluster.
+    """
+    pd.set_option('display.max_colwidth', -1)
+    keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "DBName", "Endpoint", "NumberOfNodes", 'VpcId']
+    x = [(k, v) for k,v in props.items() if k in keysToShow]
+    return pd.DataFrame(data=x, columns=["Key", "Value"])
 
 
 def main():
+    """This program manages creating and deleting a redshift cluster through 
+    infrastructure as code (IAC). The program has three options:
+    1 - to create the cluster
+    2 - to checke the status of the cluster
+    3 - to delete the cluster
+    """
+    ## load the configuration file and variables
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
     
@@ -29,7 +41,8 @@ def main():
 
     DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
     
-    c2 = boto3.resource('ec2',
+    # create AWS resources: ec2, s3, iam, and redshift 
+    ec2 = boto3.resource('ec2',
                     region_name = "us-west-2",
                     aws_access_key_id=KEY,
                     aws_secret_access_key=SECRET)
@@ -48,7 +61,7 @@ def main():
                    region_name = "us-west-2",
                    aws_access_key_id=KEY,
                    aws_secret_access_key=SECRET)
-                
+    
     option = input("Please choose an option:\n"+
                    "Enter '1' to create the redhshift cluster\n"+
                    "Enter '2' to check the status of the redshift cluster\n"+
@@ -56,67 +69,69 @@ def main():
                    "Enter an option:  ")
     
 
-    if (option == '1'):
-                  e
-    
-                  try:
-                    print('1.1 Creating a new IAM Role')
-                    dwhRole = iam.create_role(
-                                    Path = '/',
-                                    RoleName = DWH_IAM_ROLE_NAME,
-                                    Description = "Allows Refshift clusters to call AWS services",
-                                    AssumeRolePolicyDocument = json.dumps(
-                                        {'Statement': [{'Action': 'sts:AssumeRole',
-                                         'Effect': 'Allow',
-                                         'Principal': {'Service':'redshift.amazonaws.com'}}],
-                                         'Version': '2012-10-17'})
-                                        )
-                  except Exception as e:
-                    print(e)
+    if (option == '1'):  
+        # create the redshift cluster
+        try:
+            print('1.1 Creating a new IAM Role')
+            dwhRole = iam.create_role(
+                Path = '/',
+                RoleName = DWH_IAM_ROLE_NAME,
+                Description = "Allows Refshift clusters to call AWS services",
+                AssumeRolePolicyDocument = json.dumps(
+                {'Statement': [{'Action': 'sts:AssumeRole',
+                'Effect': 'Allow','Principal': {'Service':'redshift.amazonaws.com'}}],
+                'Version': '2012-10-17'}))
+        except Exception as e:
+            print(e)
         
-                  # TODO: Attach Policy
-                  print('1.2 Attaching Policy')
-                  iam.attach_role_policy(RoleName = DWH_IAM_ROLE_NAME,
-                           PolicyArn = 'arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'
+            print('1.2 Attaching Policy')
+            iam.attach_role_policy(RoleName = DWH_IAM_ROLE_NAME,
+                PolicyArn = 'arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'
                               )['ResponseMetadata']['HTTPStatusCode']
     
-                  # TODO: Get and print the IAM role ARN
-                  print('1.3 Get the IAM role ARN')
-                  roleArn = iam.get_role(RoleName = DWH_IAM_ROLE_NAME)['Role']['Arn']
-                  print(roleArn)
-        
-                  try:
-                    response = redshift.create_cluster(        
-                    # TODO: add parameters for hardware
-                    ClusterType = DWH_CLUSTER_TYPE,
-                    NodeType = DWH_NODE_TYPE, 
-                    NumberOfNodes = int(DWH_NUM_NODES),
+            print('1.3 Get the IAM role ARN')
+            roleArn = iam.get_role(RoleName = DWH_IAM_ROLE_NAME)['Role']['Arn']
+            print(roleArn)
 
-                    # TODO: add parameters for identifiers & credentials
-                    DBName = DWH_DB,
-                    ClusterIdentifier = DWH_CLUSTER_IDENTIFIER,
-                    MasterUsername = DWH_DB_USER,
-                    MasterUserPassword = DWH_DB_PASSWORD,
+        try:
+            response = redshift.create_cluster(        
+            # add parameters for hardware
+            ClusterType = DWH_CLUSTER_TYPE,
+            NodeType = DWH_NODE_TYPE, 
+            NumberOfNodes = int(DWH_NUM_NODES),
+
+            # add parameters for identifiers & credentials
+            DBName = DWH_DB,
+            ClusterIdentifier = DWH_CLUSTER_IDENTIFIER,
+            MasterUsername = DWH_DB_USER,
+            MasterUserPassword = DWH_DB_PASSWORD,
         
-                    # TODO: add parameter for role (to allow s3 access)
-                    IamRoles = [roleArn]
-                    )
-                  except Exception as e:
-                    print(e)
+            # add parameter for role (to allow s3 access)
+            IamRoles = [roleArn])
+        except Exception as e:
+            print(e)
         
     
     if (option == '2'):
-                  myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
-                  print(prettyRedshiftProps(myClusterProps)) 
-
-                  if (myClusterProps['ClusterStatus']=='available'):
-                    DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
-                    DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
-                    print("DWH_ENDPOINT :: ", DWH_ENDPOINT)
-                    print("DWH_ROLE_ARN :: ", DWH_ROLE_ARN)
+        # check the status of the redshift cluster
+        try:
+            myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+            print(prettyRedshiftProps(myClusterProps)) 
+        except Exception as e:
+            print(e)
+        if (myClusterProps['ClusterStatus']=='available'):
+            DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
+            DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
+            print("DWH_ENDPOINT :: ", DWH_ENDPOINT)
+            print("DWH_ROLE_ARN :: ", DWH_ROLE_ARN)
         
         
 
+    if (option == '3'):
+        # delete the cluster and the IAM role
+        redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
+        iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+        iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
     
         
     
