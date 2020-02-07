0468e14afc5aba4937ae74b38ead1a5ab67b193a
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-07 20:41:17 +0300

    Added documentation for etl.py

--- project-3-redshift-dwh/etl.py
+++ project-3-redshift-dwh/etl.py
@@ -6,6 +6,13 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    A function that loads the staging tables from JSON files
+    
+    Keyword arguments:
+    cur -- a cursor to allow running sql commands through the connection
+    conn -- a connection to the Postgres database
+    """
     print("Loading staging tables:")
     i = 1
     for query in copy_table_queries:
@@ -16,6 +23,13 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+    A function that loads the analytics tables from the staging tables
+    
+    Keyword arguments:
+    cur -- a cursor to allow running sql commands through the connection
+    conn -- a connection to the Postgres database
+    """
     print("Inserting into star tables:")
     i = 1
     for query in insert_table_queries:
@@ -26,9 +40,14 @@ def insert_tables(cur, conn):
 
 
 def main():
+    """Loads the configuration file and connects to the redshift cluster
+     Subsequently, it drops all the tables in the cluster and recreates them
+    """
+    # load the configuration file
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # connect to redshift cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
