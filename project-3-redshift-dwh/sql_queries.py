99af094eb672ba2cbfbecb2dd17c7c88c8d24cdf
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-08 07:23:41 +0300

    Cleanup

--- project-3-redshift-dwh/sql_queries.py
+++ project-3-redshift-dwh/sql_queries.py
@@ -4,20 +4,6 @@ import configparser
 # CONFIG
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
-    
-KEY                    = config.get('AWS','KEY')
-SECRET                 = config.get('AWS','SECRET')
-
-DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
-DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
-DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
-
-DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
-DWH_DB                 = config.get("DWH","DWH_DB")
-DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
-DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
-DWH_PORT               = config.get("DWH","DWH_PORT")
-DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
 
 ARN                    = config.get("IAM_ROLE", "ARN")
     
