7093260e8134d74f3acf02d10b3075f2c1124236
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-29 08:51:22 +0300

    Added documentation for etl.py

--- project-4-spark-data-lake/etl.py
+++ project-4-spark-data-lake/etl.py
@@ -1,4 +1,3 @@
-#import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
@@ -7,14 +6,21 @@ import pyspark.sql.functions as F
 from pyspark.sql.types import IntegerType
 
 
+# Configparser is not needed in Amazon EMR as the script is running
+# directly in the cluster
+
+#import configparser
 #config = configparser.ConfigParser()
 #config.read('dl.cfg')
-
 #os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
 #os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
+    """
+    A function that creates a spark session or returns
+    the session if it is already existing
+    """
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -23,6 +29,18 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    A function that processes song data by loading it from s3 input folder,
+    processes the data with Spark, and then loads it back to s3 output
+    folder in the form of parquet files. The song data is used to create
+    two distinct tables: songs and artists.
+    
+    Keyword arguments:
+    spark       -- The created spark session
+    input_data  -- The s3 folder path that hosts the songs' data in JSON format
+    output_data -- The s3 folder path to save the results of the ETL process in
+                   partitioned parquet files. Two tables are saved: songs and artists.
+    """
     # get filepath to song data file
     song_data = input_data+'song_data/*/*/*/*.json'
     
@@ -58,15 +76,27 @@ def process_song_data(spark, input_data, output_data):
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+    A function that processes log data by loading it from s3 input folder,
+    processes the data with Spark, and then loads it back to s3 output
+    folder in the form of parquet files. The log data is joined with the song data
+    to create three distinct tables: users, songplays, and time.
+    
+    Keyword arguments:
+    spark       -- The created spark session
+    input_data  -- The s3 folder path that hosts the logs' and songs' data in JSON format
+    output_data -- The s3 folder path to save the results of the ETL process in partitioned
+                   parquet files. Three tables are saved: users, songplays and time.
+    """
     # get filepath to log data file
     log_data = input_data+'log_data/*/*/*.json'
 
     # read log data file
     df = spark.read.json(log_data)
     
-    # filter by actions for song plays
-    df = df.where(df.page == 'NextSong')
-
+    
+    # No need to filter for page=NextSong now, this is needed to capture the most
+    # up-to-date level status (paid or free)
     # extract columns for users table    
     users_table = df.filter((col('userId').isNotNull()) 
                           & (col('userId') != ''))\
@@ -85,14 +115,9 @@ def process_log_data(spark, input_data, output_data):
                .mode('overwrite')\
                .parquet(output_data+'user/user.parquet')
     print("Saved the users table as parquet files")
-
-    # create timestamp column from original timestamp column
-    #get_timestamp = udf()
-    #df = 
     
-    # create datetime column from original timestamp column
-    #get_datetime = udf()
-    #df = 
+    # filter by actions for song plays
+    df = df.where(df.page == 'NextSong')
     
     # read in song data to use for songplays table
     song_data = input_data+'song_data/*/*/*/*.json'
@@ -110,9 +135,12 @@ def process_log_data(spark, input_data, output_data):
                                      "location",
                                      "userAgent AS user_agent"])
     
+    # convert the start_time bigint column into timestamps
+    # create a unique id for songplays
     songplays_table = songplays_table\
         .withColumn("start_time", (F.to_timestamp(songplays_table.start_time/1000)))\
         .withColumn("songplay_id", F.monotonically_increasing_id())
+    # Create year and month columns in order to save paritioned parquet files. 
     songplays_table = songplays_table\
         .withColumn("year", F.year(songplays_table.start_time))\
         .withColumn("month", F.month(songplays_table.start_time))
@@ -141,9 +169,12 @@ def process_log_data(spark, input_data, output_data):
 
 
 def main():
+    """
+    The main subroutine that creates the spark session, and then
+    processes song and log data.
+    """
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    #input_data = "data/"
     output_data = "s3://spark-ait/"
     
     process_song_data(spark, input_data, output_data)    
