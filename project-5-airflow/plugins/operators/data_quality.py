dfa9e89cc424ee9d3d0326d4717b85a41f1e07fa
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-03-17 19:21:05 +0300

    Feat: completed data quality task operator

--- project-5-airflow/plugins/operators/data_quality.py
+++ project-5-airflow/plugins/operators/data_quality.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+import logging
 
 class DataQualityOperator(BaseOperator):
 
@@ -8,15 +9,40 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 check_count=True,
+                 check_nulls=True,
+                 tables=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.check_count=check_count
+        self.check_nulls=check_nulls
+        self.tables = tables
+        self.redshift_conn_id=redshift_conn_id
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('DataQualityOperator implemented and starting...')
+        for table in self.tables:
+            if (self.check_count):
+                self.log.info(f"Checking that table: {table} has rows...")
+                redshift_hook = PostgresHook(self.redshift_conn_id)
+                records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+                if len(records) < 1 or len(records[0]) < 1:
+                    raise ValueError(f"Data quality check failed. {table} returned no results")
+                num_records = records[0][0]
+                if num_records < 1:
+                    raise ValueError(f"Data quality check failed. {table} contained 0 rows")
+                self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+                
+            if (self.check_nulls):
+                self.log.info(f"Checking that table: {table} has no null primary key records...")
+                redshift_hook = PostgresHook(self.redshift_conn_id)
+                records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table} WHERE 1 IS NULL")
+                if len(records) < 1 or len(records[0]) < 1:
+                    raise ValueError(f"Data quality check failed. {table} returned no results")
+                num_records = records[0][0]
+                if num_records > 0:
+                    raise ValueError(f"Data quality check failed. {table} contained {num_records} rows with null values")
+                self.log.info(f"Data quality on table {table} check passed with {records[0][0]} null records")
+        
--- project-5-airflow/plugins/operators/stage_redshift.py
+++ project-5-airflow/plugins/operators/stage_redshift.py
@@ -42,7 +42,7 @@ class StageToRedshiftOperator(BaseOperator):
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         
-        redshift.run("DELETE FROM {}".format(self.table))
+        #redshift.run("DELETE FROM {}".format(self.table))
         
         rendered_key = self.s3_key.format(**context)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
